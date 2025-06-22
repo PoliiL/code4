e34178421ed3c7c391a6caea6d51625200c6012a
PoliiL <slowedmusic627@gmail.com>
2025-06-23 00:04:36 +0400

    Add files via upload

---
+++ testpy (2) (1).py
@@ -0,0 +1,9 @@
+# A function that checks if a year is a leap year
+def is_leap(year):
+    return year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)
+# Example
+year = 2025
+if is_leap(year):
+    print(f"The year {year} is a leap year")
+else:
+    print(f"The year {year} is not a leap year")
